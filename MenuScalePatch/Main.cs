7332bc73b5fc1ffe609de259fa1f83dc9bbf513b
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2022-09-10 05:29:42 -0500

    initial upload

---
+++ MenuScalePatch/Main.cs
@@ -0,0 +1,48 @@
+﻿using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Player;
+using ABI_RC.Core.Savior;
+using cohtml;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+
+namespace MenuScalePatch;
+
+public class MenuScalePatch : MelonMod
+{
+    [HarmonyPatch]
+    private class HarmonyPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CVR_MenuManager), "SetScale")]
+        private static void SetQMScale(ref CohtmlView ___quickMenu, ref float ____scaleFactor)
+        {
+            //correct quickmenu - pretty much needsQuickmenuPositionUpdate()
+            Transform rotationPivot = PlayerSetup.Instance._movementSystem.rotationPivot;
+            ___quickMenu.transform.eulerAngles = new Vector3(rotationPivot.eulerAngles.x, rotationPivot.eulerAngles.y, rotationPivot.eulerAngles.z);
+            ___quickMenu.transform.position = rotationPivot.position + rotationPivot.forward * 1f * ____scaleFactor;
+        }
+
+        //ViewManager.SetScale runs once a second when it should only run when aspect ratio changes...? bug
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ViewManager), "SetScale")]
+        private static void SetMMScale(ref ViewManager __instance, ref bool ___needsMenuPositionUpdate, ref float ___scaleFactor, ref float ___cachedScreenAspectRatio, ref float ___cachedAvatarHeight)
+        {
+            //correct main menu - pretty much UpdateMenuPosition()
+            Transform rotationPivot = PlayerSetup.Instance._movementSystem.rotationPivot;
+            float num = Mathf.Abs(rotationPivot.localRotation.eulerAngles.z);
+            float settingsFloat = MetaPort.Instance.settings.GetSettingsFloat("GeneralMinimumMenuTilt");
+            if (MetaPort.Instance.isUsingVr && (num <= settingsFloat || num >= 360f - settingsFloat))
+            {
+                __instance.gameObject.transform.rotation = Quaternion.LookRotation(rotationPivot.forward, Vector3.up);
+            }
+            else
+            {
+                __instance.gameObject.transform.eulerAngles = new Vector3(rotationPivot.eulerAngles.x, rotationPivot.eulerAngles.y, rotationPivot.eulerAngles.z);
+            }
+            __instance.gameObject.transform.position = rotationPivot.position + rotationPivot.forward * 1f * ___scaleFactor;
+            ___needsMenuPositionUpdate = false;
+        }
+    }
+}
---
+++ MenuScalePatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using MenuScalePatch.Properties;
+using System.Reflection;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(MenuScalePatch))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(MenuScalePatch))]
+
+[assembly: MelonInfo(
+    typeof(MenuScalePatch.MenuScalePatch),
+    nameof(MenuScalePatch),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/MenuScalePatch"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+
+namespace MenuScalePatch.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
