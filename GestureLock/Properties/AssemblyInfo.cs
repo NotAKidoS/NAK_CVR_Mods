49563a00d3701c237ed5c492141ccba227015069
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2022-09-15 14:11:37 -0500

    dll bump

--- GestureLock/Properties/AssemblyInfo.cs
+++ GestureLock/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Reflection;
 namespace GestureLock.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.1.0";
+    public const string Version = "1.1.1";
     public const string Author = "NotAKidoS";
 }
