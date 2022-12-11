ffa50bee02d962a3030b13a1363f03fa1bbaf3a1
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2022-12-10 23:02:32 -0600

    initial
    Allows for disabling the CVRPathCamController monobehavior. Lets players to use the numpad keys without being interrupted by the path camera.
    
    Also additional setting to disable the flight bind, as that is handled by the CVRPathCamController...?

---
+++ PathCamDisabler/Main.cs
@@ -0,0 +1,52 @@
+﻿using ABI_RC.Core.Player;
+using ABI_RC.Core.IO;
+using MelonLoader;
+using UnityEngine;
+
+namespace PathCamDisabler;
+
+public class PathCamDisabler : MelonMod
+{
+    private static MelonPreferences_Category m_categoryPathCamDisabler;
+    private static MelonPreferences_Entry<bool> m_entryDisablePathCam, m_entryDisableFlightBind;
+
+    public override void OnInitializeMelon()
+    {
+        m_categoryPathCamDisabler = MelonPreferences.CreateCategory(nameof(PathCamDisabler));
+        m_entryDisablePathCam = m_categoryPathCamDisabler.CreateEntry<bool>("Disable Path Camera Controller.", true, description: "Disable path camera controller so you can use your numpad keys without funky shit.");
+        m_entryDisableFlightBind = m_categoryPathCamDisabler.CreateEntry<bool>("Disable Flight Binding (if controller off).", false, description: "Disables the flight binding as the path camera controller handles that...?");
+        m_categoryPathCamDisabler.SaveToFile(false);
+
+        foreach (var setting in m_categoryPathCamDisabler.Entries)
+        {
+            setting.OnEntryValueChangedUntyped.Subscribe(OnUpdateSettings);
+        }
+
+        MelonLoader.MelonCoroutines.Start(WaitForLocalPlayer());
+    }
+
+    System.Collections.IEnumerator WaitForLocalPlayer()
+    {
+        while (CVRPathCamController.Instance == null)
+            yield return null;
+        UpdateSettings();
+    }
+
+    private void UpdateSettings()
+    {
+        CVRPathCamController.Instance.enabled = !m_entryDisablePathCam.Value;
+    }
+
+    private void OnUpdateSettings(object arg1, object arg2) => UpdateSettings();
+
+    public override void OnUpdate()
+    {
+        if (m_entryDisablePathCam.Value && !m_entryDisableFlightBind.Value)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad5))
+            {
+                PlayerSetup.Instance._movementSystem.ToggleFlight();
+            }
+        }
+    }
+}
---
+++ PathCamDisabler/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using PathCamDisabler.Properties;
+using MelonLoader;
+using System.Reflection;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(PathCamDisabler))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(PathCamDisabler))]
+
+[assembly: MelonInfo(
+    typeof(PathCamDisabler.PathCamDisabler),
+    nameof(PathCamDisabler),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/PathCamDisabler"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+
+namespace PathCamDisabler.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
