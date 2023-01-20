ef01bd85977ce2ebb30dfc101e0bb41600f8198d
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-01-20 02:39:57 -0600

    initial release
    i built this in like 4 hours fuck

---
+++ PortableCamera-FarClipFix/HarmonyPatches.cs
@@ -0,0 +1,33 @@
+﻿using ABI_RC.Systems.Camera;
+using HarmonyLib;
+using NAK.Melons.PortableCameraAdditions.VisualMods;
+using UnityEngine;
+
+namespace NAK.Melons.PortableCameraAdditions.HarmonyPatches;
+
+[HarmonyPatch]
+internal class HarmonyPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PortableCamera), "Start")]
+    private static void Postfix_PortableCamera_Start(ref PortableCamera __instance)
+    {
+        //run mod.Setup() instead of registering full mod with icon
+        AdditionalSettings mainMod = new AdditionalSettings();
+        mainMod.Setup(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PortableCamera), "OnWorldLoaded")]
+    private static void Postfix_PortableCamera_OnWorldLoaded(Camera worldCamera)
+    {
+        AdditionalSettings.Instance?.OnWorldLoaded(worldCamera);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PortableCamera), "UpdateOptionsDisplay")]
+    private static void Postfix_PortableCamera_UpdateOptionsDisplay(ref bool ____showExpertSettings)
+    {
+        AdditionalSettings.Instance?.OnUpdateOptionsDisplay(____showExpertSettings);
+    }
+}
