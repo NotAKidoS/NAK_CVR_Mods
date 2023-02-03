fcc83aa5708cea5b3cbbeaf3f6348a3e2039397d
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-02-02 22:54:47 -0600

    init

---
+++ FuckToes/Main.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+
+namespace NAK.Melons.FuckToes;
+
+public class FuckToesMod : MelonMod
+{
+    internal const string SettingsCategory = "Fuck Toes";
+    internal static MelonPreferences_Category m_categoryFuckToes;
+    internal static MelonPreferences_Entry<bool> m_entryEnabledVR, m_entryEnabledFBT;
+    public override void OnInitializeMelon()
+    {
+        m_categoryFuckToes = MelonPreferences.CreateCategory(SettingsCategory);
+        m_entryEnabledVR = m_categoryFuckToes.CreateEntry<bool>("Enabled", true, description: "Nuke VRIK toes when in Halfbody.");
+        m_entryEnabledFBT = m_categoryFuckToes.CreateEntry<bool>("Enabled in FBT", false, description: "Nuke VRIK toes when in FBT.");
+
+        //Apply patches (i stole)
+        ApplyPatches(typeof(HarmonyPatches.VRIKPatches));
+    }
+
+    private void ApplyPatches(Type type)
+    {
+        try
+        {
+            HarmonyInstance.PatchAll(type);
+        }
+        catch (Exception e)
+        {
+            LoggerInstance.Msg($"Failed while patching {type.Name}!");
+            LoggerInstance.Error(e);
+        }
+    }
+}
---
+++ FuckToes/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using FuckToes.Properties;
+using MelonLoader;
+using System.Reflection;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.Melons.FuckToes))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.Melons.FuckToes))]
+
+[assembly: MelonInfo(
+    typeof(NAK.Melons.FuckToes.FuckToesMod),
+    nameof(NAK.Melons.FuckToes),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/FuckToes"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonOptionalDependencies("BTKUILib")]
+
+namespace FuckToes.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
