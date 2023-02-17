25fa179bd8f7acea0a8a03ee97ec18fc90cc856b
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-02-17 07:14:55 -0600

    initial release

---
+++ TrackedControllerFix/HarmonyPatches.cs
@@ -0,0 +1,44 @@
+﻿using ABI_RC.Core.Base;
+using ABI_RC.Core.Player;
+using HarmonyLib;
+using Valve.VR;
+
+namespace NAK.Melons.TrackedControllerFix.HarmonyPatches;
+
+internal class PlayerSetupPatches
+{
+    public static SteamVR_Behaviour_Pose vrLeftHandPose;
+    public static SteamVR_Behaviour_Pose vrRightHandPose;
+
+    public static SteamVR_TrackedObject vrLeftHandTracker;
+    public static SteamVR_TrackedObject vrRightHandTracker;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlayerSetup), "Start")]
+    private static void Post_PlayerSetup_Start(ref PlayerSetup __instance)
+    {
+        // Add SteamVR_TrackedObject and get SteamVR_Behaviour_Pose
+        vrLeftHandTracker = __instance.vrLeftHandTracker.AddComponent<SteamVR_TrackedObject>();
+        vrRightHandTracker = __instance.vrRightHandTracker.AddComponent<SteamVR_TrackedObject>();
+        vrLeftHandPose = __instance.vrLeftHandTracker.GetComponent<SteamVR_Behaviour_Pose>();
+        vrRightHandPose = __instance.vrRightHandTracker.GetComponent<SteamVR_Behaviour_Pose>();
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(PlayerSetup), "SetupAvatarVr")]
+    private static void Prefix_PlayerSetup_SetupAvatarVr()
+    {
+        // This is a super lazy way of doing this...
+        // but this is the best way to support DesktopVRSwitch & not redo the controller inputs
+        if (vrLeftHandTracker != null)
+        {
+            vrLeftHandTracker.SetDeviceIndex(vrLeftHandPose.GetDeviceIndex());
+            vrLeftHandPose.enabled = false;
+        }
+        if (vrRightHandTracker != null)
+        {
+            vrRightHandTracker.SetDeviceIndex(vrRightHandPose.GetDeviceIndex());
+            vrRightHandPose.enabled = false;
+        }
+    }
+}
---
+++ TrackedControllerFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using NAK.Melons.TrackedControllerFix.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.Melons.TrackedControllerFix))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.Melons.TrackedControllerFix))]
+
+[assembly: MelonInfo(
+    typeof(NAK.Melons.TrackedControllerFix.TrackedControllerFixMod),
+    nameof(NAK.Melons.TrackedControllerFix),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/TrackedControllerFix"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.Melons.TrackedControllerFix.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
