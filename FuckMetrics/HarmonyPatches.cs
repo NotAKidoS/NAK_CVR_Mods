28347e45a41ff319916bd84392c2a2fb7f9601ad
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-03-16 02:05:05 -0500

    rename mod & add settings

---
+++ FuckMetrics/HarmonyPatches.cs
@@ -0,0 +1,58 @@
+﻿using ABI_RC.Core.InteractionSystem;
+using ABI_RC.Core.Player;
+using HarmonyLib;
+
+namespace NAK.Melons.FuckMetrics.HarmonyPatches;
+
+class PlayerSetupPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlayerSetup), "Start")]
+    private static void Postfix_PlayerSetup_Start()
+    {
+        FuckMetrics.ToggleMetrics(FuckMetricsMod.EntryDisableMetrics.Value == FuckMetricsMod.SettingState.Enabled);
+        FuckMetrics.ToggleCoreUpdates(FuckMetricsMod.EntryDisableCoreUpdates.Value == FuckMetricsMod.SettingState.Enabled);
+    }
+}
+
+class CVR_MenuManagerPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CVR_MenuManager), "ToggleQuickMenu", new Type[] { typeof(bool) })]
+    private static void Postfix_CVR_MenuManager_ToggleQuickMenu(bool show)
+    {
+        if (FuckMetricsMod.EntryDisableCoreUpdates.Value == FuckMetricsMod.SettingState.Disabled) return;
+
+        if (FuckMetricsMod.EntryDisableCoreUpdates.Value == FuckMetricsMod.SettingState.MenuOnly)
+        {
+            FuckMetrics.ToggleMetrics(show);
+            FuckMetrics.ToggleCoreUpdates(show);
+        }
+        else if (show)
+        {
+            ViewManager.Instance.UpdateMetrics();
+            CVR_MenuManager.Instance.SendCoreUpdate();
+        }
+    }
+}
+
+class ViewManagerPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ViewManager), "UiStateToggle", new Type[] { typeof(bool) })]
+    private static void Postfix_ViewManager_UiStateToggle(bool show)
+    {
+        if (FuckMetricsMod.EntryDisableCoreUpdates.Value == FuckMetricsMod.SettingState.Disabled) return;
+
+        if (FuckMetricsMod.EntryDisableCoreUpdates.Value == FuckMetricsMod.SettingState.MenuOnly)
+        {
+            FuckMetrics.ToggleMetrics(show);
+            FuckMetrics.ToggleCoreUpdates(show);
+        }
+        else if (show)
+        {
+            ViewManager.Instance.UpdateMetrics();
+            CVR_MenuManager.Instance.SendCoreUpdate();
+        }
+    }
+}
--- FuckMetrics/Main.cs
+++ FuckMetrics/Main.cs
@@ -9,11 +9,18 @@ public class FuckMetricsMod : MelonMod
     public const string SettingsCategory = "FuckMetrics";
     public static readonly MelonPreferences_Category CategoryFuckMetrics = MelonPreferences.CreateCategory(SettingsCategory);
 
-    public static readonly MelonPreferences_Entry<bool> EntryDisableMetrics =
-        CategoryFuckMetrics.CreateEntry("Disable Metrics", true, description: "Disables menu metrics (FPS & Ping). Updates once on menu open if disabled.");
+    public static readonly MelonPreferences_Entry<SettingState> EntryDisableMetrics =
+        CategoryFuckMetrics.CreateEntry("Menu Metrics", SettingState.MenuOnly, description: "Disables menu metrics (FPS & Ping). Updates once on menu open if disabled.");
 
-    public static readonly MelonPreferences_Entry<bool> EntryDisableCoreUpdates =
-        CategoryFuckMetrics.CreateEntry("Disable Core Updates", true, description: "Disables menu core updates (Gamerule Icons & Debug Status). Updates once on menu open if disabled.");
+    public static readonly MelonPreferences_Entry<SettingState> EntryDisableCoreUpdates =
+        CategoryFuckMetrics.CreateEntry("Menu Core Updates", SettingState.MenuOnly, description: "Disables menu core updates (Gamerule Icons & Debug Status). Updates once on menu open if disabled.");
+
+    public enum SettingState
+    {
+        Enabled,
+        MenuOnly,
+        Disabled
+    }
 
     public override void OnInitializeMelon()
     {
@@ -27,12 +34,12 @@ public class FuckMetricsMod : MelonMod
 
     private void OnDisableMetrics(object arg1, object arg2)
     {
-        FuckMetrics.ToggleMetrics(EntryDisableMetrics.Value);
+        FuckMetrics.ToggleMetrics(EntryDisableMetrics.Value == SettingState.Enabled);
     }
 
     private void OnDisableCoreUpdates(object arg1, object arg2)
     {
-        FuckMetrics.ToggleCoreUpdates(EntryDisableCoreUpdates.Value);
+        FuckMetrics.ToggleCoreUpdates(EntryDisableMetrics.Value == SettingState.Enabled);
     }
 
     private void ApplyPatches(Type type)
