8c780bb72bc060df1a474eb72f4b723d7d3ca2cc
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-04-13 19:15:28 -0500

    init

---
+++ ClearHudNotifications/Main.cs
@@ -0,0 +1,40 @@
+﻿using ABI_RC.Core.UI;
+using MelonLoader;
+using UnityEngine;
+
+namespace NAK.Melons.ClearHudNoficiations;
+
+public class ClearHudNoficiations : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        ApplyPatches(typeof(HarmonyPatches.CohtmlHudPatches));
+    }
+
+    public override void OnUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F4))
+        {
+            ClearHudNotifications();
+        }
+    }
+
+    public static void ClearHudNotifications()
+    {
+        // sending an immediate notification clears buffer
+        CohtmlHud.Instance?.ViewDropTextImmediate("(Local) Client", "Notifications Cleared!", "Cleared Hud Notifications!");
+    }
+
+    void ApplyPatches(Type type)
+    {
+        try
+        {
+            HarmonyInstance.PatchAll(type);
+        }
+        catch (Exception e)
+        {
+            LoggerInstance.Msg($"Failed while patching {type.Name}!");
+            LoggerInstance.Error(e);
+        }
+    }
+}
---
+++ ClearHudNotifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using NAK.Melons.ClearHudNoficiations.Properties;
+using MelonLoader;
+using System.Reflection;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.Melons.ClearHudNoficiations))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.Melons.ClearHudNoficiations))]
+
+[assembly: MelonInfo(
+    typeof(NAK.Melons.ClearHudNoficiations.ClearHudNoficiations),
+    nameof(NAK.Melons.ClearHudNoficiations),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/UndoPropButton"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.Melons.ClearHudNoficiations.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
