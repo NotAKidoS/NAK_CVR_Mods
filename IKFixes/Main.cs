a47e1d8ce739310d1e529c70aaf9256e7b08f9cd
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-04-13 19:35:06 -0500

    format

--- IKFixes/Main.cs
+++ IKFixes/Main.cs
@@ -8,7 +8,7 @@ public class IKFixesMod : MelonMod
     public static readonly MelonPreferences_Category CategoryIKFixes = MelonPreferences.CreateCategory(SettingsCategory);
 
     public static readonly MelonPreferences_Entry<bool> EntryUseFakeRootAngle =
-        CategoryIKFixes.CreateEntry("Use Fake Root Angle", false, description: "Emulates maxRootAngle. This fixes feet pointing in direction of head when looking around.");
+        CategoryIKFixes.CreateEntry("Use Fake Root Angle", true, description: "Emulates maxRootAngle. This fixes feet pointing in direction of head when looking around.");
 
     public static readonly MelonPreferences_Entry<float> EntryFakeRootAngleLimit =
         CategoryIKFixes.CreateEntry("Fake Root Angle Limit", 25f, description: "Specifies the maximum angle the lower body can have relative to the head when rotating.");
