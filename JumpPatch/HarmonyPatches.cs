6fd838e88b06a191c4c8e93fab2c0d6c90e4f0d6
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-05-02 12:57:28 -0500

    [JumpPatch] Fix PRM conflict

--- JumpPatch/HarmonyPatches.cs
+++ JumpPatch/HarmonyPatches.cs
@@ -7,9 +7,17 @@ namespace NAK.JumpPatch.HarmonyPatches;
 class MovementSystemPatches
 {
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(MovementSystem), "Update")]
-    private static void Prefix_MovementSystem_Update(ref bool ____isGrounded)
+    [HarmonyPatch(typeof(MovementSystem), nameof(MovementSystem.Update))]
+    private static void Prefix_MovementSystem_Update(ref bool ____isGrounded, ref bool __state)
     {
+        __state = CVRInputManager.Instance.jump;
         CVRInputManager.Instance.jump = CVRInputManager.Instance.jump && ____isGrounded;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MovementSystem), nameof(MovementSystem.Update))]
+    private static void Postfix_MovementSystem_Update(ref bool __state)
+    {
+        CVRInputManager.Instance.jump = __state;
     }
 }
