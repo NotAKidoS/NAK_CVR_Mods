b667c32c63f5fbcf935ee5bf330737328344be90
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-05-02 12:57:48 -0500

    [ThirdPerson] Use AssemblyInfo.cs

---
+++ ThirdPerson/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using NAK.ThirdPerson.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.ThirdPerson))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.ThirdPerson))]
+
+[assembly: MelonInfo(
+    typeof(NAK.ThirdPerson.ThirdPerson),
+    nameof(NAK.ThirdPerson),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/ThirdPerson"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(ConsoleColor.DarkMagenta)]
+
+namespace NAK.ThirdPerson.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.3";
+    public const string Author = "Davi & NotAKidoS";
+}
