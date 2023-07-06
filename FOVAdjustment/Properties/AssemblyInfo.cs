6e447c5cfffa30c4f056bc0b6a18c269b8047952
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-07-06 14:51:29 -0500

    [FOVAdjustment] Initial release

---
+++ FOVAdjustment/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using MelonLoader;
+using NAK.FOVAdjustment.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.FOVAdjustment))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.FOVAdjustment))]
+
+[assembly: MelonInfo(
+    typeof(NAK.FOVAdjustment.FOVAdjustment),
+    nameof(NAK.FOVAdjustment),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/FOVAdjustment"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+
+namespace NAK.FOVAdjustment.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
