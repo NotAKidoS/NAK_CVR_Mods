16e44f7c35ba6cb577ff05e6ff795147436327d4
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-07-12 12:09:00 -0500

    [NoDepthOnlyFlat] Initial testing

---
+++ NoDepthOnlyFlat/Main.cs
@@ -0,0 +1,73 @@
+﻿using ABI_RC.Core.Base;
+using ABI_RC.Core.Player;
+using MelonLoader;
+using System.Reflection;
+using System.Web.Services.Description;
+using ABI_RC.Systems.Camera;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+namespace NAK.NoDepthOnlyFlat;
+
+public class NoDepthOnlyFlat : MelonMod
+{
+    public static readonly MelonPreferences_Category Category =
+        MelonPreferences.CreateCategory(nameof(NoDepthOnlyFlat));
+
+    public static readonly MelonPreferences_Entry<bool> EntryUseDepthOnPlayerCamera =
+        Category.CreateEntry("Use Depth On Player Camera", false, description: "Toggle depth texture on player cameras.");
+
+    public static readonly MelonPreferences_Entry<bool> EntryUseDepthOnPortableCamera =
+        Category.CreateEntry("Use Depth On Portable Camera", false, description: "Toggle depth texture on portable camera.");
+
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch(
+            typeof(PlayerSetup).GetMethod(nameof(PlayerSetup.Start)),
+            postfix: new HarmonyLib.HarmonyMethod(typeof(NoDepthOnlyFlat).GetMethod(nameof(OnPlayerSetupStart_Postfix), BindingFlags.NonPublic | BindingFlags.Static))
+        );
+
+        HarmonyInstance.Patch(
+            typeof(PortableCamera).GetMethod(nameof(PortableCamera.Start)),
+            postfix: new HarmonyLib.HarmonyMethod(typeof(NoDepthOnlyFlat).GetMethod(nameof(OnPortableCameraStart_Postfix), BindingFlags.NonPublic | BindingFlags.Static))
+        );
+        
+        foreach (MelonPreferences_Entry setting in Category.Entries)
+            setting.OnEntryValueChangedUntyped.Subscribe(OnSettingsChanged);
+    }
+
+    public static void OnSettingsChanged(object oldValue = null, object newValue = null)
+    {
+        SetPlayerSetupCameraDepthTextureMode(EntryUseDepthOnPlayerCamera.Value);
+        SetPortableCameraDepthTextureMode(EntryUseDepthOnPortableCamera.Value);
+    }
+
+    private static void SetPlayerSetupCameraDepthTextureMode(bool useDepth)
+    {
+        if (PlayerSetup.Instance != null)
+        {
+            PlayerSetup.Instance.desktopCamera.GetComponent<Camera>().depthTextureMode = useDepth ? DepthTextureMode.Depth : DepthTextureMode.None;
+            PlayerSetup.Instance.vrCamera.GetComponent<Camera>().depthTextureMode = useDepth ? DepthTextureMode.Depth : DepthTextureMode.None;
+        }
+    }
+
+    private static void SetPortableCameraDepthTextureMode(bool useDepth)
+    {
+        if (PortableCamera.Instance != null)
+        {
+            PortableCamera.Instance._camera.depthTextureMode = useDepth ? DepthTextureMode.Depth : DepthTextureMode.None;
+        }
+    }
+
+    // Lazy way to set settings on start
+
+    private static void OnPlayerSetupStart_Postfix()
+    {
+        SetPlayerSetupCameraDepthTextureMode(EntryUseDepthOnPlayerCamera.Value);
+    }
+
+    private static void OnPortableCameraStart_Postfix()
+    {
+        SetPortableCameraDepthTextureMode(EntryUseDepthOnPortableCamera.Value);
+    }
+}
---
+++ NoDepthOnlyFlat/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using MelonLoader;
+using NAK.NoDepthOnlyFlat.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.NoDepthOnlyFlat))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.NoDepthOnlyFlat))]
+
+[assembly: MelonInfo(
+    typeof(NAK.NoDepthOnlyFlat.NoDepthOnlyFlat),
+    nameof(NAK.NoDepthOnlyFlat),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/NoDepthOnlyFlat"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+
+namespace NAK.NoDepthOnlyFlat.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
