e61f119f32a2f71757303be4421651d2c016e289
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-07-26 10:38:14 -0500

    [FuckToes] Update for 2023r171
    Fixed a few setting issues too.

--- FuckToes/Main.cs
+++ FuckToes/Main.cs
@@ -1,34 +1,56 @@
-﻿using MelonLoader;
+﻿using ABI_RC.Core.Savior;
+using ABI_RC.Systems.IK;
+using MelonLoader;
+using RootMotion.FinalIK;
+using System.Reflection;
 
 namespace NAK.FuckToes;
 
 public class FuckToes : MelonMod
 {
-    public static readonly MelonPreferences_Category Category = 
+    internal static MelonLogger.Instance Logger;
+
+    public static readonly MelonPreferences_Category Category =
         MelonPreferences.CreateCategory(nameof(FuckToes));
 
-    public static readonly MelonPreferences_Entry<bool> EntryEnabledVR = 
-        Category.CreateEntry("Enabled", true, description: "Nuke VRIK toes when in Halfbody.");
+    public static readonly MelonPreferences_Entry<bool> EntryEnabledVR =
+        Category.CreateEntry("Enabled in HalfBody", true, description: "Nuke VRIK toes when in Halfbody.");
 
-    public static readonly MelonPreferences_Entry<bool> EntryEnabledFBT = 
-        Category.CreateEntry("Enabled in FBT", false, description: "Nuke VRIK toes when in FBT.");
+    public static readonly MelonPreferences_Entry<bool> EntryEnabledFBT =
+        Category.CreateEntry("Enabled in FBT", true, description: "Nuke VRIK toes when in FBT.");
 
     public override void OnInitializeMelon()
     {
-        //Apply patches (i stole)
-        ApplyPatches(typeof(HarmonyPatches.VRIKPatches));
+        Logger = LoggerInstance;
+        HarmonyInstance.Patch(
+            typeof(VRIK).GetMethod(nameof(VRIK.AutoDetectReferences)),
+            prefix: new HarmonyLib.HarmonyMethod(typeof(FuckToes).GetMethod(nameof(OnVRIKAutoDetectReferences_Prefix), BindingFlags.NonPublic | BindingFlags.Static))
+        );
     }
 
-    private void ApplyPatches(Type type)
+    private static void OnVRIKAutoDetectReferences_Prefix(ref VRIK __instance)
     {
         try
         {
-            HarmonyInstance.PatchAll(type);
+            // Must be PlayerLocal layer and in VR
+            if (__instance.gameObject.layer != 8 || !MetaPort.Instance.isUsingVr)
+                return;
+
+            // Not in FBT, and not enabled, perish
+            if (!IKSystem.Instance.BodySystem.FBTActive() && !EntryEnabledVR.Value)
+                return;
+
+            // In FBT, and not enabled in fbt, perish
+            if (IKSystem.Instance.BodySystem.FBTActive() && !EntryEnabledFBT.Value)
+                return;
+
+            __instance.references.leftToes = null;
+            __instance.references.rightToes = null;
         }
         catch (Exception e)
         {
-            LoggerInstance.Msg($"Failed while patching {type.Name}!");
-            LoggerInstance.Error(e);
+            Logger.Error($"Error during the patched method {nameof(OnVRIKAutoDetectReferences_Prefix)}");
+            Logger.Error(e);
         }
     }
 }
--- FuckToes/Properties/AssemblyInfo.cs
+++ FuckToes/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using FuckToes.Properties;
+﻿using NAK.FuckToes.Properties;
 using MelonLoader;
 using System.Reflection;
 
@@ -20,12 +20,13 @@ using System.Reflection;
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
 [assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
-[assembly: MelonOptionalDependencies("BTKUILib")]
+[assembly: MelonColor(255, 255, 200, 0)]
+[assembly: MelonAuthorColor(255, 158, 21, 32)]
 [assembly: HarmonyDontPatchAll]
 
-namespace FuckToes.Properties;
+namespace NAK.FuckToes.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Author = "NotAKidoS";
 }
