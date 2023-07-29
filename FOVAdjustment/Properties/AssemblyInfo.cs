c4a7f51be31768b06115fe6ce09f9b068183620a
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-07-29 00:00:27 -0500

    [FOVAdjustment] Fixes for 2023r171

--- FOVAdjustment/Properties/AssemblyInfo.cs
+++ FOVAdjustment/Properties/AssemblyInfo.cs
@@ -20,10 +20,14 @@ using System.Reflection;
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
 [assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonOptionalDependencies("UIExpansionKit")]
+[assembly: MelonColor(255, 125, 125, 125)]
+[assembly: MelonAuthorColor(255, 158, 21, 32)]
+[assembly: HarmonyDontPatchAll]
 
 namespace NAK.FOVAdjustment.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Author = "NotAKidoS";
 }
