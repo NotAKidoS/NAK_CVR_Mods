374ab6c11eb14f6eba641cebac32b8b3136980fb
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2023-12-28 02:45:59 -0600

    [ThirdPerson] Added restriction when world has Zoom disabled.

--- ThirdPerson/CameraLogic.cs
+++ ThirdPerson/CameraLogic.cs
@@ -1,15 +1,10 @@
-﻿using ABI_RC.Core.Base;
-using ABI_RC.Core.Player;
+﻿using ABI_RC.Core.Player;
 using ABI_RC.Core.Savior;
 using ABI_RC.Core.Util.Object_Behaviour;
-using Aura2API;
-using BeautifyEffect;
 using System.Collections;
-using System.Reflection;
 using ABI_RC.Core;
+using ABI.CCK.Components;
 using UnityEngine;
-using UnityEngine.AzureSky;
-using UnityEngine.Rendering.PostProcessing;
 
 namespace NAK.ThirdPerson;
 
@@ -38,17 +33,13 @@ internal static class CameraLogic
         get => _state;
         set
         {
-            _state = value;
+            _state = !CheckIsRestricted() && value;
             if (_state) _storedCamMask = _desktopCam.cullingMask;
             _desktopCam.cullingMask = _state ? 0 : _storedCamMask;
             _thirdpersonCam.gameObject.SetActive(_state);
         }
     }
-
-    private static bool _setupPostProcessing;
-    private static readonly FieldInfo _ppResourcesFieldInfo = typeof(PostProcessLayer).GetField("m_Resources", BindingFlags.NonPublic | BindingFlags.Instance);
-    private static readonly FieldInfo _ppOldResourcesFieldInfo = typeof(PostProcessLayer).GetField("m_OldResources", BindingFlags.NonPublic | BindingFlags.Instance);
-
+    
     internal static IEnumerator SetupCamera()
     {
         yield return new WaitUntil(() => PlayerSetup.Instance);
@@ -71,41 +62,46 @@ internal static class CameraLogic
 
     internal static void CopyPlayerCamValues()
     {
-        Camera ourCamComponent = _thirdpersonCam;
         Camera activePlayerCam = PlayerSetup.Instance.GetActiveCamera().GetComponent<Camera>();
-        if (ourCamComponent == null || activePlayerCam == null) 
+        if (_thirdpersonCam == null || activePlayerCam == null) 
             return;
         
         ThirdPerson.Logger.Msg("Copying active camera settings & components.");
+        CVRTools.CopyToDestCam(activePlayerCam, _thirdpersonCam);
+
+        if (!CheckIsRestricted()) 
+            return;
         
-        CVRTools.CopyToDestCam(_thirdpersonCam, activePlayerCam);
+        ThirdPerson.Logger.Msg("Third person camera is restricted by the world.");
+        State = false;
     }
 
     internal static void RelocateCam(CameraLocation location, bool resetDist = false)
     {
-        _thirdpersonCam.transform.rotation = _desktopCam.transform.rotation;
+        Transform thirdPersonCam = _thirdpersonCam.transform;
+        thirdPersonCam.rotation = _desktopCam.transform.rotation;
         if (resetDist) ResetDist();
         switch (location)
         {
             case CameraLocation.FrontView:
-                _thirdpersonCam.transform.localPosition = new Vector3(0, 0.015f, 1f - _dist) * _scale;
-                _thirdpersonCam.transform.localRotation = new Quaternion(0, 180, 0, 0);
+                thirdPersonCam.localPosition = new Vector3(0, 0.015f, 1f - _dist) * _scale;
+                thirdPersonCam.localRotation = new Quaternion(0, 180, 0, 0);
                 CurrentLocation = CameraLocation.FrontView;
                 break;
             case CameraLocation.RightSide:
-                _thirdpersonCam.transform.localPosition = new Vector3(0.3f, 0.015f, -1.5f + _dist) * _scale;
-                _thirdpersonCam.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                thirdPersonCam.localPosition = new Vector3(0.3f, 0.015f, -1.5f + _dist) * _scale;
+                thirdPersonCam.localRotation = new Quaternion(0, 0, 0, 0);
                 CurrentLocation = CameraLocation.RightSide;
                 break;
             case CameraLocation.LeftSide:
-                _thirdpersonCam.transform.localPosition = new Vector3(-0.3f, 0.015f, -1.5f + _dist) * _scale;
-                _thirdpersonCam.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                thirdPersonCam.localPosition = new Vector3(-0.3f, 0.015f, -1.5f + _dist) * _scale;
+                thirdPersonCam.localRotation = new Quaternion(0, 0, 0, 0);
                 CurrentLocation = CameraLocation.LeftSide;
                 break;
             case CameraLocation.Default:
             default:
-                _thirdpersonCam.transform.localPosition = new Vector3(0, 0.015f, -1.5f + _dist) * _scale;
-                _thirdpersonCam.transform.localRotation = new Quaternion(0, 0, 0, 0);
+                thirdPersonCam.localPosition = new Vector3(0, 0.015f, -1.5f + _dist) * _scale;
+                thirdPersonCam.localRotation = new Quaternion(0, 0, 0, 0);
                 CurrentLocation = CameraLocation.Default;
                 break;
         }
@@ -116,4 +112,6 @@ internal static class CameraLogic
     internal static void ScrollDist(float sign) { _dist += sign * 0.25f; RelocateCam(CurrentLocation); }
     internal static void AdjustScale(float height) { _scale = height; RelocateCam(CurrentLocation); }
     internal static void CheckVRMode() { if (MetaPort.Instance.isUsingVr) State = false; }
+    private static bool CheckIsRestricted()
+        => !CVRWorld.Instance.enableZoom;
 }
