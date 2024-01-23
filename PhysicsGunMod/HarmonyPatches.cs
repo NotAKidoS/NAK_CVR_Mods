d155ea546e55f6da7b429dddefab90ae9846c390
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-01-22 21:53:03 -0600

    [PhysicsGunMod] PhysGun test

---
+++ PhysicsGunMod/HarmonyPatches.cs
@@ -0,0 +1,20 @@
+﻿using ABI_RC.Systems.InputManagement;
+using HarmonyLib;
+using NAK.PhysicsGunMod.Components;
+using UnityEngine;
+
+namespace NAK.PhysicsGunMod.HarmonyPatches;
+
+internal static class CVRInputManagerPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CVRInputManager), nameof(CVRInputManager.Update))]
+    private static void Postfix_CVRInputManager_Update(ref CVRInputManager __instance)
+    {
+        if (PhysicsGunInteractionBehavior.Instance == null)
+            return;
+        
+        if (PhysicsGunInteractionBehavior.Instance.UserRotation)
+            __instance.lookVector = Vector2.zero;
+    }
+}
---
+++ PhysicsGunMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using NAK.PhysicsGunMod.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.PhysicsGunMod))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.PhysicsGunMod))]
+
+[assembly: MelonInfo(
+    typeof(NAK.PhysicsGunMod.PhysicsGunMod),
+    nameof(NAK.PhysicsGunMod),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/PhysicsGunMod"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(255, 241, 200, 82)]
+[assembly: MelonAuthorColor(255, 114, 17, 25)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.PhysicsGunMod.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
