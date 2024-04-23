f12fb89fc03e28b2459cc8de24b477dfc0a98bd2
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-04-23 17:08:45 -0500

    [ShadowCloneFallback] Fixed option condition being inverted

--- ShadowCloneFallback/Main.cs
+++ ShadowCloneFallback/Main.cs
@@ -10,18 +10,18 @@ public class ShadowCloneFallback : MelonMod
     private static readonly MelonPreferences_Category Category =
         MelonPreferences.CreateCategory(SettingsCategory);
     
-    private static readonly MelonPreferences_Entry<bool> EntryEnabled =
-        Category.CreateEntry("Enabled", true, description: "Toggle ShadowCloneFallback entirely.");
+    private static readonly MelonPreferences_Entry<bool> EntryUseFallbackClones =
+        Category.CreateEntry("Use Fallback Clones", true, description: "Toggle ShadowCloneFallback entirely.");
     
     public override void OnInitializeMelon()
     {
-        ShadowCloneUtils.s_UseShaderClones = EntryEnabled.Value;
-        EntryEnabled.OnEntryValueChanged.Subscribe(OnEnabledChanged);
+        ShadowCloneUtils.s_UseShaderClones = EntryUseFallbackClones.Value;
+        EntryUseFallbackClones.OnEntryValueChanged.Subscribe(OnEnabledChanged);
     }
     
     private void OnEnabledChanged(bool _, bool __)
     {
-        ShadowCloneUtils.s_UseShaderClones = EntryEnabled.Value;
-        LoggerInstance.Msg($"ShadowCloneUtils.s_UseShaderClones is now {(EntryEnabled.Value ? "enabled" : "disabled")}.");
+        ShadowCloneUtils.s_UseShaderClones = !EntryUseFallbackClones.Value;
+        LoggerInstance.Msg($"ShadowCloneFallback is now {(EntryUseFallbackClones.Value ? "enabled" : "disabled")}.");
     }
 }
