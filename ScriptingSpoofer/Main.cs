be814f6be54766bd273889760afa2ad775a1022c
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-05-28 09:38:00 -0500

    [ScriptingSpoofer] Initial build

---
+++ ScriptingSpoofer/Main.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection;
+using ABI_RC.API;
+using ABI_RC.Core.Networking;
+using ABI_RC.Core.Networking.API.Responses;
+using ABI_RC.Systems.GameEventSystem;
+using HarmonyLib;
+using MelonLoader;
+using Random = UnityEngine.Random;
+
+namespace NAK.ScriptingSpoofer;
+
+public class ScriptingSpoofer : MelonMod
+{
+    public static readonly MelonPreferences_Category Category =
+        MelonPreferences.CreateCategory(nameof(ScriptingSpoofer));
+
+    public static readonly MelonPreferences_Entry<bool> EntryEnabled =
+        Category.CreateEntry("Enabled", true, description: "Toggle scripting spoofer.");
+    
+    public static readonly MelonPreferences_Entry<bool> EntryCensorUsername =
+        Category.CreateEntry("Censor Username", true, description: "Censor username. Toggle to randomize username instead.");
+
+    private static string spoofedUsername;
+    private static string spoofedUserId;
+    
+    private static readonly char[] CensorChars = {'!', '@', '#', '$', '%', '^', '&', '*'};
+
+    public override void OnInitializeMelon()
+    {
+        ApplyPatches(typeof(PlayerApiPatches));
+        
+        // Regenerate spoofed data on login
+        CVRGameEventSystem.Authentication.OnLogin.AddListener(GenerateRandomSpoofedData);
+    }
+    
+    private void ApplyPatches(Type type)
+    {
+        try
+        {
+            HarmonyInstance.PatchAll(type);
+        }
+        catch (Exception e)
+        {
+            LoggerInstance.Msg($"Failed while patching {type.Name}!");
+            LoggerInstance.Error(e);
+        }
+    }
+
+    private static void GenerateRandomSpoofedData(UserAuthResponse _) // we get manually
+    {
+        spoofedUsername = EntryCensorUsername.Value ? GenerateCensoredUsername() : GenerateRandomUsername();
+        spoofedUserId = Guid.NewGuid().ToString();
+    }
+    
+    private static string GenerateCensoredUsername()
+    {
+        var originalUsername = AuthManager.Username;
+        var usernameArray = originalUsername.ToCharArray();
+
+        for (var i = 0; i < usernameArray.Length; i++)
+            if (Random.value > 0.7f) usernameArray[i] = CensorChars[Random.Range(0, CensorChars.Length)];
+
+        var modifiedUsername = new string(usernameArray);
+        string[] prefixes = { "xX", "_", Random.Range(10, 99).ToString() };
+        string[] suffixes = { "Xx", "_", Random.Range(10, 99).ToString() };
+
+        var prefix = prefixes[Random.Range(0, prefixes.Length)];
+        var suffix = suffixes[Random.Range(0, suffixes.Length)];
+
+        return prefix + modifiedUsername + suffix;
+    }
+    
+    private static string GenerateRandomUsername()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        char[] username = new char[Random.Range(5, 15)];
+        for (int i = 0; i < username.Length; i++)
+            username[i] = chars[Random.Range(0, chars.Length)];
+
+        return new string(username);
+    }
+
+    private static class PlayerApiPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(LocalPlayerApi), nameof(LocalPlayerApi.Username), MethodType.Getter)]
+        [HarmonyPatch(typeof(PlayerApiBase), nameof(PlayerApiBase.Username), MethodType.Getter)]
+        private static bool GetSpoofedUsername(ref PlayerApiBase __instance, ref string __result)
+        {
+            if (__instance.IsRemote) return true;
+            if (!EntryEnabled.Value) return true;
+
+            __result = spoofedUsername;
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(LocalPlayerApi), nameof(LocalPlayerApi.UserID), MethodType.Getter)]
+        [HarmonyPatch(typeof(PlayerApiBase), nameof(PlayerApiBase.UserID), MethodType.Getter)]
+        private static bool GetSpoofedUserId(ref PlayerApiBase __instance, ref string __result)
+        {
+            if (__instance.IsRemote) return true;
+            if (!EntryEnabled.Value) return true;
+
+            __result = spoofedUserId;
+            return false;
+        }
+    }
+}
---
+++ ScriptingSpoofer/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using NAK.ScriptingSpoofer.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.ScriptingSpoofer))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.ScriptingSpoofer))]
+
+[assembly: MelonInfo(
+    typeof(NAK.ScriptingSpoofer.ScriptingSpoofer),
+    nameof(NAK.ScriptingSpoofer),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/ScriptingSpoofer"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.ScriptingSpoofer.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
