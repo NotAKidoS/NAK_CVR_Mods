9464995557cd9efbd53ae50ac9c09ede6d080f6b
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-06-10 00:03:41 -0500

    [AASDefaultProfileFix] Initial release

---
+++ AASDefaultProfileFix/Main.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using ABI_RC.Core.Util.AnimatorManager;
+using ABI.CCK.Scripts;
+using HarmonyLib;
+using MelonLoader;
+
+namespace NAK.AASDefaultProfileFix;
+
+public class AASDefaultProfileFix : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch(
+            typeof(CVRAdvancedAvatarSettings).GetMethod(nameof(CVRAdvancedAvatarSettings.LoadProfile),
+                BindingFlags.Public | BindingFlags.Instance), // earliest callback (why the fuck are you public)
+            prefix: new HarmonyMethod(typeof(AASDefaultProfileFix).GetMethod(nameof(OnAttemptLoadAASProfile),
+                BindingFlags.NonPublic | BindingFlags.Static))
+        );
+    }
+
+    // when default profile is selected the defaultProfileName string is empty
+    // so it does not load/apply anything- we will fix by forcing LoadDefault when this is detected
+    private static bool OnAttemptLoadAASProfile(string profileName, AvatarAnimatorManager animatorManager,
+        ref CVRAdvancedAvatarSettings __instance)
+    {
+        if (!string.IsNullOrEmpty(profileName) // LoadDefault sets defaultProfileName as ""
+            && profileName != "default") // CVRAdvancedSettingsFile defines initial default as "default"
+            return true;
+
+        __instance.LoadDefault(animatorManager);
+        return false;
+    }
+}
---
+++ AASDefaultProfileFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using NAK.AASDefaultProfileFix.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.AASDefaultProfileFix))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.AASDefaultProfileFix))]
+
+[assembly: MelonInfo(
+    typeof(NAK.AASDefaultProfileFix.AASDefaultProfileFix),
+    nameof(NAK.AASDefaultProfileFix),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/AASDefaultProfileFix"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.AASDefaultProfileFix.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
