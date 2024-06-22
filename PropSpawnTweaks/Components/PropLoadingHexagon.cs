cafd24bd2f36cfc72d3818d92015ef15c4fa6ec3
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-06-22 01:49:44 -0500

    [PropSpawnTweaks] Initial builds

---
+++ PropSpawnTweaks/Components/PropLoadingHexagon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace NAK.PropSpawnTweaks.Components;
+
+public class PropLoadingHexagon : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer _hexRenderer;
+    [SerializeField] private TMPro.TextMeshPro _loadingText;
+    [SerializeField] private Transform[] _hexTransforms;
+    private float _scale;
+    
+    private void Update()
+    {
+        if (_scale < 1f)
+        {
+            _scale += Time.deltaTime * 4f;
+            transform.GetChild(0).localScale = Vector3.one * _scale;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            // give slightly different rotation to each hexagon
+            _hexTransforms[i].Rotate(Vector3.up, 30f * Time.deltaTime * (i + 1));
+        }
+    }
+    
+    public void SetLoadingText(string text)
+        => _loadingText.text = text; // SetAllDirty
+
+    public void SetLoadingShape(float value)
+        => _hexRenderer.SetBlendShapeWeight(0, value);
+}
---
+++ PropSpawnTweaks/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using NAK.PropSpawnTweaks.Properties;
+using MelonLoader;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.PropSpawnTweaks))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.PropSpawnTweaks))]
+
+[assembly: MelonInfo(
+    typeof(NAK.PropSpawnTweaks.PropSpawnTweaksMod),
+    nameof(NAK.PropSpawnTweaks),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/PropSpawnTweaks"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(255, 181, 137, 236)]
+[assembly: MelonAuthorColor(255, 158, 21, 32)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.PropSpawnTweaks.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
