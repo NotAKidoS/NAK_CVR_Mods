a4781c18e7845555d817551bf3cbe64a9b4b93c3
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-06-24 02:54:53 -0500

    [DropPropTweak] Initial release

---
+++ DropPropTweak/Main.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using ABI_RC.Core.Player;
+using ABI_RC.Core.Util;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace NAK.DropPropTweak;
+
+public class DropPropTweakMod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch( // make drop prop actually usable
+            typeof(PlayerSetup).GetMethod(nameof(PlayerSetup.DropProp),
+                BindingFlags.Public | BindingFlags.Instance),
+            new HarmonyMethod(typeof(DropPropTweakMod).GetMethod(nameof(OnDropProp),
+                BindingFlags.NonPublic | BindingFlags.Static))
+        );
+    }
+    
+    private static bool OnDropProp(string propGuid, ref PlayerSetup __instance)
+    {
+        Vector3 position = __instance.activeCam.transform.position + __instance.GetPlayerForward() * 1.5f; // 1f -> 1.5f
+
+        if (Physics.Raycast(position,
+                __instance.CharacterController.GetGravityDirection(), // align with gravity, not player up
+                out RaycastHit raycastHit, 4f, __instance.dropPlacementMask))
+        {
+            // native method passes false, so DropProp doesn't align with gravity :)
+            CVRSyncHelper.SpawnProp(propGuid, raycastHit.point.x, raycastHit.point.y, raycastHit.point.z, true);
+            return false;
+        }
+
+        // unlike original, we will still spawn prop even if raycast fails, giving the method actual utility :3
+
+        // hack- we want to align with *our* rotation, not affecting gravity
+        Vector3 ogGravity = __instance.CharacterController.GetGravityDirection();
+        __instance.CharacterController.gravity = -__instance.transform.up; // align with our rotation
+
+        // spawn prop with useTargetLocationGravity false, so it pulls our gravity dir we've modified
+        CVRSyncHelper.SpawnProp(propGuid, position.x, position.y, position.z, false);
+
+        __instance.CharacterController.gravity = ogGravity; // restore gravity
+        return false;
+    }
+
+
+}
---
+++ DropPropTweak/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using NAK.DropPropTweak.Properties;
+using MelonLoader;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.DropPropTweak))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.DropPropTweak))]
+
+[assembly: MelonInfo(
+    typeof(NAK.DropPropTweak.DropPropTweakMod),
+    nameof(NAK.DropPropTweak),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidOnSteam/NAK_CVR_Mods/tree/main/DropPropTweak"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(255, 246, 25, 99)] // red-pink
+[assembly: MelonAuthorColor(255, 158, 21, 32)] // red
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.DropPropTweak.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "Exterrata & NotAKidoS";
+}
