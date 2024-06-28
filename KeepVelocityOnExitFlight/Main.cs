f052ec7c8d38b4ec3f2c8f25d5c19fbadd0b204f
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2024-06-27 21:00:48 -0500

    [KeepVelocityOnExitFlight] Initial release

---
+++ KeepVelocityOnExitFlight/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using ABI_RC.Core.Util.AnimatorManager;
+using ABI_RC.Systems.Movement;
+using ABI.CCK.Scripts;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace NAK.KeepVelocityOnExitFlight;
+
+public class KeepVelocityOnExitFlightMod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch(
+            typeof(BetterBetterCharacterController).GetMethod(nameof(BetterBetterCharacterController.ChangeFlight),
+                BindingFlags.Public | BindingFlags.Instance),
+            prefix: new HarmonyMethod(typeof(KeepVelocityOnExitFlightMod).GetMethod(nameof(Prefix_OnChangeFlight),
+                BindingFlags.NonPublic | BindingFlags.Static)),
+            postfix: new HarmonyMethod(typeof(KeepVelocityOnExitFlightMod).GetMethod(nameof(Postfix_OnChangeFlight),
+                BindingFlags.NonPublic | BindingFlags.Static))
+        );
+    }
+    
+    // ReSharper disable once RedundantAssignment
+    private static void Prefix_OnChangeFlight(ref BetterBetterCharacterController __instance, ref Vector3 __state)
+    {
+        __state = __instance.GetVelocity();
+    }
+    
+    private static void Postfix_OnChangeFlight(ref BetterBetterCharacterController __instance, ref Vector3 __state)
+    {
+        if (__instance.FlightAllowedInWorld && !__instance.IsFlying()) 
+            __instance.SetVelocity(__state);
+    }
+}
---
+++ KeepVelocityOnExitFlight/README.md
@@ -0,0 +1,14 @@
+# KeepVelocityOnExitFlight
+
+Keeps the player's velocity when exiting flight mode. Makes it possible to fling yourself like in Garry's Mod.
+
+---
+
+Here is the block of text where I tell you this mod is not affiliated with or endorsed by ABI. 
+https://documentation.abinteractive.net/official/legal/tos/#7-modding-our-games
+
+> This mod is an independent creation not affiliated with, supported by, or approved by Alpha Blend Interactive. 
+
+> Use of this mod is done so at the user's own risk and the creator cannot be held responsible for any issues arising from its use.
+
+> To the best of my knowledge, I have adhered to the Modding Guidelines established by Alpha Blend Interactive.
