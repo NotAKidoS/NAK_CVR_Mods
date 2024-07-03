e77959a8a946efef4e8f79080a3a801c4d602aa0
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2024-07-03 00:13:17 -0500

    HeadLookLockingInputFix: Initial release

---
+++ HeadLookLockingInputFix/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using ABI_RC.Core.Player;
+using ABI_RC.Core.Savior;
+using HarmonyLib;
+using MelonLoader;
+
+namespace NAK.HeadLookLockingInputFix;
+
+public class HeadLookLockingInputFixMod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch(
+            typeof(InputManager).GetMethod(nameof(InputManager.HandleMenuHeadLook),
+                BindingFlags.NonPublic | BindingFlags.Instance),
+            prefix: new HarmonyMethod(typeof(HeadLookLockingInputFixMod).GetMethod(nameof(OnPreHandleMenuHeadLook),
+                BindingFlags.NonPublic | BindingFlags.Static))
+        );
+    }
+    
+    private static bool OnPreHandleMenuHeadLook() 
+        => !MetaPort.Instance.isUsingVr; // only execute in Desktop
+}
---
+++ HeadLookLockingInputFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using NAK.HeadLookLockingInputFix.Properties;
+using System.Reflection;
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(NAK.HeadLookLockingInputFix))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(NAK.HeadLookLockingInputFix))]
+
+[assembly: MelonInfo(
+    typeof(NAK.HeadLookLockingInputFix.HeadLookLockingInputFixMod),
+    nameof(NAK.HeadLookLockingInputFix),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/NotAKidoS/NAK_CVR_Mods/tree/main/HeadLookLockingInputFix"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(255, 125, 126, 129)]
+[assembly: MelonAuthorColor(255, 158, 21, 32)]
+[assembly: HarmonyDontPatchAll]
+
+namespace NAK.HeadLookLockingInputFix.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
