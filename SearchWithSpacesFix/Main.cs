d0c8298074c4dcfc089ccb34ed8b8bd7e0b9cedf
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2024-10-29 23:56:10 -0500

    SearchWithSpacesFix: initial release

---
+++ SearchWithSpacesFix/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using ABI_RC.Core.InteractionSystem;
+using HarmonyLib;
+using MelonLoader;
+
+namespace NAK.SearchWithSpacesFix;
+
+public class SearchWithSpacesFixMod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch(
+            typeof(ViewManager).GetMethod(nameof(ViewManager.GetSearchResults),
+                BindingFlags.Public | BindingFlags.Instance),
+            prefix: new HarmonyMethod(typeof(SearchWithSpacesFixMod).GetMethod(nameof(OnPreGetSearchResults),
+                BindingFlags.NonPublic | BindingFlags.Static))
+        );
+    }
+
+    // this is so crazy
+
+    private static void OnPreGetSearchResults(ref string searchTerm)
+        => searchTerm = searchTerm.Replace(" ", "_");
+
+    // this is so crazy
+}
