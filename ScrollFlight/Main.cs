0720ab508dd960ed55e5e624cad8d0f321e9ff25
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2024-12-02 20:07:01 -0600

    ScrollFlight: fixed error spam on launch, added reset on exit flight option

--- ScrollFlight/Main.cs
+++ ScrollFlight/Main.cs
@@ -23,6 +23,10 @@ public class ScrollFlightMod : MelonMod
         Category.CreateEntry("use_scroll_flight", true,  
             "Use Scroll Flight", description: "Toggle Scroll Flight.");
     
+    private static readonly MelonPreferences_Entry<bool> EntryResetOnExitFlight =
+        Category.CreateEntry("reset_on_exit_flight", false,  
+            "Reset On Exit Flight", description: "Reset Scroll Flight speed on exit flight.");
+    
     #endregion Melon Preferences
 
     #region Private Fields
@@ -39,14 +43,29 @@ public class ScrollFlightMod : MelonMod
         CVRWorld.GameRulesUpdated += OnApplyMovementSettings; // thank you kafe for using actions
     }
     
+    bool wasFlying = false;
+    
     // stole from LucMod :3
     public override void OnUpdate()
     {
         if (!EntryUseScrollFlight.Value) 
             return;
         
-        if (BetterBetterCharacterController.Instance == null
-            || !BetterBetterCharacterController.Instance.IsFlying()
+        if (BetterBetterCharacterController.Instance == null)
+            return;
+        
+        bool isFlying = BetterBetterCharacterController.Instance.IsFlying();
+        
+        if (EntryResetOnExitFlight.Value 
+            && (wasFlying && !isFlying))
+        {
+            BetterBetterCharacterController.Instance.worldFlightSpeedMultiplier = _originalWorldFlightSpeedMultiplier;
+            _currentWorldFlightSpeedMultiplier = _originalWorldFlightSpeedMultiplier;
+        }
+        
+        wasFlying = isFlying;
+        
+        if (!isFlying
             || Input.GetKey(KeyCode.Mouse2) // scroll zoom
             || Input.GetKey(KeyCode.LeftControl) // third person / better interact desktop
             || Cursor.lockState != CursorLockMode.Locked) // unity explorer / in menu
@@ -58,7 +77,7 @@ public class ScrollFlightMod : MelonMod
     
     private static void AdjustFlightModifier(float adjustValue)
     {
-        _currentWorldFlightSpeedMultiplier = Math.Max(0f, _currentWorldFlightSpeedMultiplier + adjustValue);
+        _currentWorldFlightSpeedMultiplier = Mathf.Max(0f, _currentWorldFlightSpeedMultiplier + adjustValue);
 
         if (_currentWorldFlightSpeedMultiplier <= 0f)
         {
