e027829103f6cc9f52e75fecd54209af10adff41
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2024-12-27 17:55:39 -0600

    TwoWayMute: initial release

---
+++ TwoWayMute/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using ABI_RC.Systems.Communications.Audio.Components;
+using HarmonyLib;
+using MelonLoader;
+
+namespace NAK.TwoWayMute;
+
+public class TwoWayMuteMod : MelonMod
+{
+    public override void OnInitializeMelon()
+    {
+        HarmonyInstance.Patch(
+            typeof(Comms_ParticipantPipeline).GetMethod(nameof(Comms_ParticipantPipeline.SetFlowControlState),
+                BindingFlags.NonPublic | BindingFlags.Instance),
+            prefix: new HarmonyMethod(typeof(TwoWayMuteMod).GetMethod(nameof(OnSetFlowControlState),
+                BindingFlags.NonPublic | BindingFlags.Static))
+        );
+    }
+    
+    private static void OnSetFlowControlState(
+        ref bool state, 
+        Comms_ParticipantPipeline __instance)
+        => state &= !__instance._selfModerationMute;
+}
