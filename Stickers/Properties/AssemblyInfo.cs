bf89ee24f597af1eaf3505c1ae67c5ccac183983
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2025-04-03 04:01:28 -0500

    [Stickers] bump version, fixes for 2025r179

--- Stickers/Properties/AssemblyInfo.cs
+++ Stickers/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Reflection;
 namespace NAK.Stickers.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.0.8";
+    public const string Version = "1.0.9";
     public const string Author = "NotAKidoS, SketchFoxsky";
 }
