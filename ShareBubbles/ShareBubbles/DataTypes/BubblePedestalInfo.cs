9133c6b16154f0b0ad051d5bec9d174618b582e2
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2025-04-03 04:34:30 -0500

    [ShareBubbles] Remove todo notes

--- ShareBubbles/ShareBubbles/DataTypes/BubblePedestalInfo.cs
+++ ShareBubbles/ShareBubbles/DataTypes/BubblePedestalInfo.cs
@@ -5,6 +5,6 @@ public class BubblePedestalInfo
     public string Name;
     public string ImageUrl;
     public string AuthorId;
-    public bool IsPermitted; // TODO: awaiting luc to fix not being true for private shared (only true for public)
-    public bool IsPublic; // TODO: awaiting luc to add
+    public bool IsPermitted;
+    public bool IsPublic;
 }
