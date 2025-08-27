30c069388c0e76414f2e152df9413d7c5be29311
NotAKidoS <37721153+NotAKidoS@users.noreply.github.com>
2025-08-27 01:23:56 -0500

    [PathCamDisabler] Fixes for 2025r180

--- PathCamDisabler/Properties/AssemblyInfo.cs
+++ PathCamDisabler/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     downloadLink: "https://github.com/NotAKidoS/NAK_CVR_Mods/tree/main/PathCamDisabler"
 )]
 
-[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonGame("ChilloutVR", "ChilloutVR")]
 [assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
 [assembly: MelonColor(255, 246, 25, 99)] // red-pink
@@ -27,6 +27,6 @@ using System.Reflection;
 namespace NAK.PathCamDisabler.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
     public const string Author = "NotAKidoS";
 }
